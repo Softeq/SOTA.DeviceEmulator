107e080f82d91baaa5cc8863b0971558cd659a61
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2019-06-19 22:36:36 +0300

    Simplify universal package version generation
    JIRA: SOTA-15

--- tools/SOTA.DeviceEmulator.Build/BuildMetadata.cs
+++ tools/SOTA.DeviceEmulator.Build/BuildMetadata.cs
@@ -10,15 +10,7 @@ class BuildMetadata
     {
         BuildVersion = gitVersion.FullSemVer;
         ReleaseGitTag = $"v{gitVersion.FullSemVer}";
-        var universalSemver = new string(gitVersion.FullSemVer.ToLowerInvariant().TakeWhile(c => c != '+').ToArray());
-        var universalSemverParts = universalSemver.Split('-');
-        var index = 1;
-        foreach (var part in universalSemverParts.Skip(index))
-        {
-            universalSemverParts[index] = part.Replace(".", string.Empty);
-            index++;
-        }
-        UniversalPackageVersion = string.Join("-", universalSemverParts);
+        UniversalPackageVersion = gitVersion.NuGetVersionV2;
         ReleaseType = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(gitVersion.PreReleaseLabel ?? "stable");
     }
 
