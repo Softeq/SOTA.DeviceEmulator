5e81170f7d30f613e761479228859da416413e1a
Konstantin Smolyakov <smolyakoff.ko@gmail.com>
2019-07-04 14:46:35 +0300

    Update id scope for dev environment

--- src/SOTA.DeviceEmulator/Properties/Settings.Designer.cs
+++ src/SOTA.DeviceEmulator/Properties/Settings.Designer.cs
@@ -51,7 +51,7 @@ namespace SOTA.DeviceEmulator.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
             "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>dev=0ne00064748</string>\r\n</ArrayOfString>")]
+            "tring>dev=0ne00067465</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection DeviceProvisioningServiceIdScopeMap {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["DeviceProvisioningServiceIdScopeMap"]));
